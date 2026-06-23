3e4f2724531dd694084ea2acb2650750df7447ae
darinwalshy <darinwalshy@gmail.com>
2026-06-23 12:51:33 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'boilerplate-pwa-v1.0';
+const REPO_NAME = '/Boilerplate';
+
+const ASSETS = [
+  `${REPO_NAME}/`,
+  `${REPO_NAME}/index.html`,
+  `${REPO_NAME}/manifest.json`
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+  self.skipWaiting(); 
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch event: NETWORK FIRST with immediate cache fallback
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes(self.location.origin)) {
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches.match(event.request).then((response) => {
+          return response || caches.match(`${REPO_NAME}/index.html`);
+        });
+      })
+    );
+  }
+});
